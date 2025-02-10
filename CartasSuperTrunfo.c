6acac372bb1966009b351f3c2d1d2b9ba8aa9949
wlima17 <williamlima1786@gmail.com>
2025-02-10 22:43:13 +0000

    Correção de nome de variável

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,7 +10,7 @@ int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
 
-    char pais[10];
+    char estado[10];
     char codigo_da_carta[5]; //Definido limite de caracteres, para ficar dentro do padrão proposto.
     char nome_cidade[30];
     int populacao;
@@ -23,8 +23,8 @@ int main() {
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
     //Função printf será utilizada sempre antes da captura de cada dado, para orientar usuário de como proceder.
-    printf("Digite a sigla do País: \n"); 
-    scanf("%s", &pais);
+    printf("Digite o Estado: \n"); 
+    scanf("%s", &estado);
 
     printf("Lembrete: o código da carta de ser a inicial do País mais o número da carta, ex:B01\n");
     printf("Digite o código da carta: \n"); 
@@ -32,12 +32,16 @@ int main() {
 
     printf("Digite o nome da cidade: \n"); 
     scanf("%s", &nome_cidade);
+    
     printf("Digite a populção dessa cidade: \n"); 
     scanf("%d", &populacao);
+
     printf("Digite a área dessa cidade: \n"); 
     scanf("%f", &area);
+
     printf("Digite o PIB dessa cidade: \n"); 
     scanf("%f", &pib);
+
     printf("Quantos pontos turísticos essa cidade possui: \n"); 
     scanf("%d", &pontos_turisticos);
 
@@ -46,7 +50,7 @@ int main() {
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
     //Nesse bloco a função printf irá mostrar os dados capturados referente aos atributos das cartas.
-    printf("País: %s\n", pais);
+    printf("Estado: %s\n", estado);
     printf("Código da carta: %s\n", codigo_da_carta);
     printf("Nome da cidade: %s\n", nome_cidade);
     printf("População: %d\n", populacao);
